f6e0d452553c67ba074e740e6e153364de707e8b
tejarshaarigila <arigilatejarsha@gmail.com>
2024-10-23 16:50:31 -0500

    Update main_fedavg.py

--- main_fedavg.py
+++ main_fedavg.py
@@ -139,6 +139,7 @@ def main():
 
     # Load pre-partitioned data
     client_datasets_per_round = load_partitions(
+        dataset=args.dataset,
         num_clients=args.num_clients,
         num_rounds=args.num_rounds,
         partition_dir=args.partition_dir,
