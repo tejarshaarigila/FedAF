6a5477b21a79eff7208ca883c01214995c7b5856
tejarshaarigila <arigilatejarsha@gmail.com>
2024-10-23 14:01:47 -0500

    Update main_fedaf.py

--- main_fedaf.py
+++ main_fedaf.py
@@ -46,7 +46,7 @@ def parse_args():
     parser.add_argument('--global_steps', type=int, default=500, help='Global training steps')
     parser.add_argument('--num_clients', type=int, default=10, help='Number of clients')
     parser.add_argument('--alpha', type=float, default=0.1, help='Dirichlet distribution parameter for non-IID data')
-    parser.add_argument('--honesty_ratio', type=int, default=1, help='Ratio of honest clients (0 to 1)')
+    parser.add_argument('--honesty_ratio', type=float, default=1.0, help='Ratio of honest clients (0 to 1)')
     parser.add_argument('--device', type=str, default='cpu', choices=['cpu', 'cuda'], help='Device to use: cpu or cuda')
     parser.add_argument('--lr_img', type=float, default=1.0, help='Learning rate for images')
     
