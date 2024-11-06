019b35135bfa9cc61cd00a5cf1077fb47de8889c
tejarshaarigila <arigilatejarsha@gmail.com>
2024-11-06 11:36:15 -0800

    Update client_fedavg.py

--- client/client_fedavg.py
+++ client/client_fedavg.py
@@ -1,126 +1,60 @@
 # client_fedavg.py
 
 import torch
-from torch.utils.data import DataLoader
-import torch.nn.functional as F
 import torch.optim as optim
-from utils.utils import get_network
+from torch.utils.data import DataLoader
+from utils.utils_fedavg import get_network
 import logging
-import os
+
+# Configure logging
+logger = logging.getLogger(__name__)
 
 class Client:
     def __init__(self, client_id, train_data, args):
-        """
-        Initializes a federated learning client.
-
-        Args:
-            client_id (int): Unique identifier for the client.
-            train_data (Subset): Subset of the dataset assigned to the client.
-            args (Namespace): Parsed arguments containing configurations.
-        """
         self.client_id = client_id
         self.args = args
-        self.device = args.device
-        self.model = get_network(
-            model_name=args.model,
-            channel=args.channel,
-            num_classes=args.num_classes,
-            im_size=args.im_size,
-            device=self.device
-        )
-        self.train_loader = DataLoader(
-            train_data,
-            batch_size=args.batch_size,
-            shuffle=True,
-            num_workers=0  # Ensure num_workers=0 for multiprocessing
-        )
-        self.criterion = torch.nn.CrossEntropyLoss()
-        self.optimizer = optim.SGD(self.model.parameters(), lr=args.lr)
+        self.train_data = train_data  # Already a Dataset or Subset
+        self.train_loader = DataLoader(self.train_data, batch_size=args.batch_size, shuffle=True)
+        self.local_model = self.initialize_model()
+        logger.info("Client %d initialized with model: %s", client_id, args.model)
 
-        # Set up a dedicated logger for the client
-        self.logger = logging.getLogger(f'FedAvg.Client{self.client_id}')
-        self.setup_logger()
+    def initialize_model(self):
+        im_size = self.args.im_size
+        channel = self.args.channel
+        num_classes = self.args.num_classes
 
-    def setup_logger(self):
-        """
-        Sets up the logger for the client to log to a separate file.
-        """
-        log_dir = "/home/t914a431/log/client_logs/"
-        os.makedirs(log_dir, exist_ok=True)
-        client_log_file = os.path.join(log_dir, f'client_{self.client_id}.log')
-
-        # Prevent adding multiple handlers if already added
-        if not self.logger.handlers:
-            file_handler = logging.FileHandler(client_log_file)
-            console_handler = logging.StreamHandler()
-
-            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
-            file_handler.setFormatter(formatter)
-            console_handler.setFormatter(formatter)
-
-            self.logger.addHandler(file_handler)
-            self.logger.addHandler(console_handler)
-            self.logger.setLevel(logging.INFO)
-
-    def set_model(self, model_state_dict):
-        """
-        Sets the client's model state to the global model state and resets the optimizer.
-        """
-        model_state_dict = {key: value.to(self.device) for key, value in model_state_dict.items()}
-        self.model.load_state_dict(model_state_dict)
-        self.logger.info("Client %d: Model state updated from global model.", self.client_id)
+        model = get_network(
+            model=self.args.model,
+            channel=channel,
+            num_classes=num_classes,
+            im_size=im_size
+        ).to(self.args.device)
         
-        # Reset optimizer to align with the new model state
-        self.optimizer = optim.SGD(self.model.parameters(), lr=self.args.lr)
-        self.logger.info("Client %d: Optimizer state reset after model update.", self.client_id)
+        logger.info("Local model initialized for client %d.", self.client_id)
+        return model
 
-    def update_train_data(self, new_train_data):
-        """
-        Updates the client's training data and reinitializes the DataLoader.
-        """
-        self.train_data = new_train_data
-        self.train_loader = DataLoader(
-            self.train_data,
-            batch_size=self.args.batch_size,
-            shuffle=True,
-            num_workers=0  # Ensure num_workers=0 for multiprocessing
-        )
-        self.logger.info("Client %d: Training data updated for new round.", self.client_id)
-        
+    def set_model(self, global_model):
+        self.local_model.load_state_dict(global_model)
+        logger.info("Client %d set the global model.", self.client_id)
+
     def train(self):
-        """
-        Trains the local model on the client's data.
+        """Train the client's model on local data."""
+        self.local_model.train()
+        optimizer = optim.SGD(self.local_model.parameters(), lr=self.args.lr)
+        criterion = torch.nn.CrossEntropyLoss()
 
-        Returns:
-            dict: State dictionary of the trained model.
-        """
-        if len(self.train_loader.dataset) == 0:
-            self.logger.info(f"Client {self.client_id}: No data to train on. Skipping.")
-            return None
-            
-        self.logger.info("Client %d: Starting local training.", self.client_id)
-        self.model.train()
-        for epoch in range(1, self.args.local_epochs + 1):
-            epoch_loss = 0.0
-            correct = 0
-            total = 0
-            for data, target in self.train_loader:
-                data, target = data.to(self.device), target.to(self.device)
-                self.optimizer.zero_grad()
-                output = self.model(data)
-                loss = self.criterion(output, target)
+        for epoch in range(self.args.local_epochs):
+            epoch_loss = 0
+            for images, labels in self.train_loader:
+                images, labels = images.to(self.args.device), labels.to(self.args.device)
+                optimizer.zero_grad()
+                outputs = self.local_model(images)
+                loss = criterion(outputs, labels)
                 loss.backward()
-                self.optimizer.step()
+                optimizer.step()
+                epoch_loss += loss.item()
 
-                epoch_loss += loss.item() * data.size(0)
-                _, predicted = torch.max(output.data, 1)
-                total += target.size(0)
-                correct += (predicted == target).sum().item()
+            avg_loss = epoch_loss / len(self.train_loader)
+            logger.info("Client %d completed epoch %d with average loss: %.4f", self.client_id, epoch + 1, avg_loss)
 
-            avg_loss = epoch_loss / len(self.train_loader.dataset)
-            accuracy = 100 * correct / total
-            self.logger.info("Client %d: Epoch %d/%d - Loss: %.4f, Accuracy: %.2f%%",
-                             self.client_id, epoch, self.args.local_epochs, avg_loss, accuracy)
-
-        self.logger.info("Client %d: Local training completed.", self.client_id)
-        return self.model.state_dict()
+        return self.local_model.state_dict()
